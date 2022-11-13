7c547e26e540ea4df931603df6546f79936b7aa2
Ramon Rodrigues <ramonrwx@gmail.com>
2022-11-13 15:00:29 -0300

    add solution for name-badge

---
+++ elixir/name-badge/lib/name_badge.ex
@@ -0,0 +1,8 @@
+defmodule NameBadge do
+  def print(id, name, department) do
+    department = if department, do: department, else: "owner"
+    id = if id, do: "[#{id}] - ", else: ""
+
+    id <> "#{name} - #{String.upcase(department)}"
+  end
+end
